f3bf16f29db4d6413270a55e040e64471e07a6d1
Daria <82046@mail.ru>
2022-10-09 20:36:41 +0300

    Task 10 solution

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Input three-digit number:");
+int N = Convert.ToInt32(Console.ReadLine());
+N = (N%100)/10;
+Console.WriteLine($"you've been looking for {N}");
