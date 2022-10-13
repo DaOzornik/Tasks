3924b2ed8bcd253d75a14820589f54776d279bb6
Daria <82046@mail.ru>
2022-10-13 23:26:08 +0300

    Task 19 solution

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,7 +5,7 @@
 32679 -> 6
 */
 
-Console.WriteLine("Input number:");
+/* Console.WriteLine("Input number:");
 int N = Convert.ToInt32(Console.ReadLine());
 
 if(N >= 99)
@@ -16,6 +16,20 @@ if(N >= 99)
 else
 {
     Console.WriteLine("there is no third digit");
+}
+*/
+
+Console.WriteLine("Input number:");
+int n = Convert.ToInt32(Console.ReadLine());
+string nText = Convert.ToString(n);
+
+if (nText.Length > 2)
+{
+Console.WriteLine("Третья цифра - " + nText[2]);
+}
+else
+{
+Console.WriteLine("Третьей цифры нет");
 }
 
 
