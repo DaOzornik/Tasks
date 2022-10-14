b341d6b5fea3ec096a4ec01ec42cee4263019a7e
Daria <82046@mail.ru>
2022-10-14 18:22:15 +0300

    Task 23 solution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,6 +4,10 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+
+
+
+
            Console.WriteLine("Input first number:");
 
            int a = Convert.ToInt32(Console.ReadLine());
---
+++ Task23/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+//Console.WriteLine("Enter number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int number = 5;
+//Console.WriteLine(Math.Pow(number, 3));
+//Console.WriteLine($"{number}  в кубе {number*number*number}");
+
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+//int value = 3;
+for (int power = 3; power <= 99; power++)
+{
+ Console.WriteLine(Math.Pow(number, power));
+}
+ Console.WriteLine();
+
+
