c3ee589d91b41671b76f067ba981b6a1566015a3
Daria <82046@mail.ru>
2022-10-14 18:43:01 +0300

    Task 21 solution

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -5,16 +5,12 @@
 5 -> 1, 8, 27, 64, 125
 */
 
-//Console.WriteLine("Enter number");
-//int number = Convert.ToInt32(Console.ReadLine());
-//int number = 5;
-//Console.WriteLine(Math.Pow(number, 3));
-//Console.WriteLine($"{number}  в кубе {number*number*number}");
+
 
 Console.WriteLine("Enter number");
 int number = Convert.ToInt32(Console.ReadLine());
 //int value = 3;
-for (int power = 3; power <= 99; power++)
+for (int power = 3; power <= 10; power++)
 {
  Console.WriteLine(Math.Pow(number, power));
 }
